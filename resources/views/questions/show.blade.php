71998b69ffe294f69f986cee916ec73eca01d758
Bayurzx <bayurzx@gmail.com>
2021-08-19 12:14:58 +0100

    Fontawesome option 2

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -5,27 +5,39 @@
     <div class="row justify-content-center pt-4">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header"><div class="d-flex align-items-center">
-                    <h1>{{$question->title}}</h1>
-                    <div class="ml-auto"><a href="{{ route('questions.index') }}" class="btn btn-outline-secondary"> <strong>👈</strong> All Questions</a></div>
-                </div>
-            </div>
-
                 <div class="card-body">
-                    {!! $question->body_html !!}
-                    <div class="float-right">
-                        <span class="text-muted">Answered {{ $question->created_date }} </span>
-                        <div class="media">
-                            <a href="{{$question->user->url}}" class="pr-2">
-                                <img src="{{ $question->user->avatar }}" alt="">
-                            </a>
-                            <div class="media-body mt-2">
-                                <a href="{{$question->user->url}}">{{$question->user->name}} </a>
+                    <div class="card-title"><div class="d-flex align-items-center">
+                        <h1>{{$question->title}}</h1>
+                        <div class="ml-auto"><a href="{{ route('questions.index') }}" class="btn btn-outline-secondary"> <strong>👈</strong> All Questions</a></div>
+                    </div>
+                    
+                    <hr>
+                    
+                    <div class="media">
+                        <div class="d-flex flex-column vote-controls">
+                            <a href="#" class="vote-up" title="This question is useful">Vote Up</a>
+                            <span class="votes-count">1238</span>
+                            <a href="#" class="vote-down off" title="This question is not useful">Vote Down</a>
+                            <a href="" class="favorite" title="Click to ark as favorite question(Click again to undo)">Favorite<span class="favorites-count">123</span></a>
+                        </div>
+                        <div class="media-body">
+                            {!! $question->body_html !!}
+                            <div class="float-right">
+                                <span class="text-muted">Answered {{ $question->created_date }} </span>
+                                <div class="media">
+                                    <a href="{{$question->user->url}}" class="pr-2">
+                                        <img src="{{ $question->user->avatar }}" alt="">
+                                    </a>
+                                    <div class="media-body mt-2">
+                                        <a href="{{$question->user->url}}">{{$question->user->name}} </a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-
                 </div>
+            </div>
+
             </div>
         </div>
     </div>
